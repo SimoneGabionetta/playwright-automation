1fc4af927a442ee421085e410ec433b6f8e9de35
SimoneGabionetta <smgabionetta@yahoo.com.br>
2025-04-18 11:24:47 +0100

    first-test

---
+++ tests/home.spec.ts
@@ -0,0 +1,7 @@
+import {test, expect} from '@playwright/test'
+
+test('application must be live', async({page}) =>{
+  //steps
+  await page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=account/login')
+  await expect(page).toHaveTitle('Account Login')
+})
