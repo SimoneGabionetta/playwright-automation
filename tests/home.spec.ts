0169decff12160f9ae894ff6ebf492a89dc45fee
SimoneGabionetta <smgabionetta@yahoo.com.br>
2025-04-18 11:38:06 +0100

    feat: enable and disable headless

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -4,4 +4,5 @@ test('application must be live', async({page}) =>{
   //steps
   await page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=account/login')
   await expect(page).toHaveTitle('Account Login')
+  await page.waitForTimeout(3000)
 })
